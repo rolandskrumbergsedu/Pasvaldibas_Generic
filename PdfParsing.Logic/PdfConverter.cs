06749178565e9fcd71432d343421b95e9a0ccb02
KRUROL <rolands.krumbergs@if.lv>
2016-12-13 16:19:38 +0200

    Doc to pdf conversion added

---
+++ PdfParsing.Logic/PdfConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+
+namespace PdfParsing.Logic
+{
+    public static class PdfConverter
+    {
+        public static void Convert(string basePath)
+        {
+            var files = Directory.GetFiles(basePath, "*.doc", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                ConvertDocFileToPdf(file);
+            }
+
+            files = Directory.GetFiles(basePath, "*.docx", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                ConvertDocxFileToPdf(file);
+            }
+        }
+
+        public static void ConvertDocFileToPdf(string file)
+        {
+            var fileName = file.Replace(".doc", string.Empty);
+
+            DoConversion(file, fileName);
+        }
+
+        public static void ConvertDocxFileToPdf(string file)
+        {
+            var fileName = file.Replace(".docx", string.Empty);
+
+            DoConversion(file, fileName);
+        }
+
+        public static void DoConversion(string file, string outputFileName)
+        {
+            var appWord = new Application();
+            if (appWord.Documents == null) return;
+            
+            var wordDocument = appWord.Documents.Open(file);
+            var pdfDocName = outputFileName + ".pdf";
+            if (wordDocument != null)
+            {
+                wordDocument.ExportAsFixedFormat(pdfDocName,
+                    WdExportFormat.wdExportFormatPDF);
+                wordDocument.Close();
+            }
+            appWord.Quit();
+        }
+    }
+}
---
+++ PdfParsing.Results/Converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PdfParsing.Logic;
+
+namespace PdfParsing.Results
+{
+    /// <summary>
+    /// Summary description for Converter
+    /// </summary>
+    [TestClass]
+    public class Converter
+    {
+        [TestMethod]
+        public void ConvertFiles()
+        {
+            PdfConverter.Convert(@"C:\Work_misc\Protokoli\Aizpute");
+        }
+    }
+}
